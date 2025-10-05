97da6332c4ddab9c2b000983eb7e66c600471c81
Ahmed Zedan <ahmedhamdizedan@gmail.com>
2025-10-05 23:01:56 +0300

    fix build target

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  checkOtpRestrictions,
+  sendOtp,
+  trackOtpRequests,
+  validateRegistrationData,
+} from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
+
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validateRegistrationData(req.body, "user");
+    const { name, email } = req.body;
+
+    const existingUser = await prisma.users.findUnique({ where: email });
+    if (existingUser) {
+      return next(
+        new ValidationError("User already exists with this email address")
+      );
+    }
+
+    await checkOtpRestrictions(email, next);
+    await trackOtpRequests(email, next);
+    await sendOtp(name, email, "user-activation-mail");
+
+    res
+      .status(200)
+      .json({
+        message: "OTP sent to mail successfully, please verify your Account",
+      });
+  } catch (error) {
+    return next(error);
+  }
+};
---
+++ apps/auth-service/src/main.ts
@@ -0,0 +1,44 @@
+
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import router from './routes/auth.router';
+import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
+
+const swaggerDocument = require('./swagger-output.json');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/docs-json', (req, res) => {
+  res.json(swaggerDocument);
+});
+
+app.use('/api', router);
+
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`Auth service is running at http://localhost:${port}`);
+  console.log(`Swagger Docs available at http://localhost:${port}/api-docs`);
+});
+
+server.on('error', (err) => {
+  console.log('Server Error:', err);
+});
